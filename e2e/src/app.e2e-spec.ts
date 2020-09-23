5b92a8aa6ae1e61eb9442e478dcd7b7ffde0668d
GitterBuy <116578133+GitterBuy@users.noreply.github.com>
2020-09-23 07:01:12 +0000

    Changed variable name.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -16,4 +16,8 @@ describe('workspace-project App', () => {
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
