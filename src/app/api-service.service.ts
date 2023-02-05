c5cc8090ea078dddc3b2b6813b53ad3bea7412e2
GitterBuy <116578133+GitterBuy@users.noreply.github.com>
2023-02-05 16:25:10 +0000

    change config file

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -5,4 +5,32 @@ import { Observable } from 'rxjs';
 
 
 export class Taxonomy {
-    
+    Access: string;
+    FCA: string;
+    FINMA: string;
+    Industry: string;
+    CollateralizedAsset: string;
+    CollateralizedAssetType: string;
+    CollateralType: string;
+    CollateralInfo: string;
+}
+
+export class Rating {
+    public Weiss: Weiss;
+}
+export class Weiss {
+    TechnologyAdoptionRating: string;
+     MarketPerformanceRating: string;
+}
+export class Data {
+    Id: string;
+    Url: string;
+    ImageUrl: string;
+    ContentCreatedOn: number;
+    Name: string;
+    Symbol: string;
+    CoinName: string;
+    FullName: string;
+    Description: string;
+    AssetTokenStatus: string;
+  
