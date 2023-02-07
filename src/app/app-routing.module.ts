bb18337617e233cb0bdfc89f50c98a65d361692b
GitterBuy <116578133+GitterBuy@users.noreply.github.com>
2023-02-07 06:26:23 +0000

    Update quiet.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CoinsRepositoryComponent } from './coins-repository/coins-reposit
+import { CoinsRepositoryComponent } from './coins-repository/coins-repository.component';
+
+const routes: Routes = [{
+  path: '',
+  component: CoinsRepositoryComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
