9aaf273e1c79543c0c65cdc7b5c23fc86b4a8323
GitterBuy <116578133+GitterBuy@users.noreply.github.com>
2023-02-12 22:10:57 +0000

    chore(package): update standard to version 9.0.0

--- src/app/interceptors/with-credentials.interceptor.ts
+++ src/app/interceptors/with-credentials.interceptor.ts
@@ -1,2 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/
+import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class WithCredentialsInterceptor  implements HttpInterceptor{
+
+  constructor() {}
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // enable credentials
+    return next.handle(req.clone({
+      withCredentials: true
+    }));
+  }
+
+}
